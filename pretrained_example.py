1a0e915887a6b5099828c1c3b4be8a1f165ec386
DanielTakeshi <takeshidanny@gmail.com>
2022-02-08 09:49:18 -0500

    gitignore and updated pretrained script

--- pretrained_example.py
+++ pretrained_example.py
@@ -14,14 +14,21 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
+import time
+
 
 def main():
+    start = time.time()
+
     # Initialize TensorFlow.
+    print('Initialize TF...')
     tflib.init_tf()
 
-    # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    # Load pre-trained network. NOTE(daniel): had to change this to pickle load.
+    # url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
+    # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    print(f'Loading pretrained... (time: {time.time()-start:0.2f}s)')
+    with open('pretrained/karras2019stylegan-ffhq-1024x1024.pkl', 'rb') as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
@@ -30,11 +37,12 @@ def main():
     # Print network details.
     Gs.print_layers()
 
-    # Pick latent vector.
-    rnd = np.random.RandomState(5)
+    # Pick latent vector. NOTE(daniel): can change integer in RandomState
+    rnd = np.random.RandomState(3)
     latents = rnd.randn(1, Gs.input_shape[1])
 
     # Generate image.
+    print(f'Generating image ... (time: {time.time()-start:0.2f}s)')
     fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
     images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
@@ -42,6 +50,8 @@ def main():
     os.makedirs(config.result_dir, exist_ok=True)
     png_filename = os.path.join(config.result_dir, 'example.png')
     PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    print(f'Done! See {png_filename} (time: {time.time()-start:0.2f}s)')
+
 
 if __name__ == "__main__":
     main()
