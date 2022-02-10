16b6342ac30a1ecf1a154055baf0ee7efecf0f00
Daniel Seita <dseita@seuss.ri.cmu.edu>
2022-02-10 09:54:51 -0500

    Adding slurm scripts

--- pretrained_example.py
+++ pretrained_example.py
@@ -16,6 +16,10 @@ import dnnlib.tflib as tflib
 import config
 import time
 
+# Might be machine dependent.
+HEAD = '/data/dseita/stylegan1'
+GAN = 'karras2019stylegan-ffhq-1024x1024.pkl'
+
 
 def main():
     start = time.time()
@@ -28,7 +32,8 @@ def main():
     # url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
     # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
     print(f'Loading pretrained... (time: {time.time()-start:0.2f}s)')
-    with open('pretrained/karras2019stylegan-ffhq-1024x1024.pkl', 'rb') as f:
+    net_name = os.path.join(HEAD, GAN)
+    with open(net_name, 'rb') as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
@@ -38,19 +43,21 @@ def main():
     Gs.print_layers()
 
     # Pick latent vector. NOTE(daniel): can change integer in RandomState
+    batch_size = 10
     rnd = np.random.RandomState(3)
-    latents = rnd.randn(1, Gs.input_shape[1])
+    latents = rnd.randn(batch_size, Gs.input_shape[1])
 
     # Generate image.
     print(f'Generating image ... (time: {time.time()-start:0.2f}s)')
     fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
     images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
-    # Save image.
+    # Save images.
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
-    print(f'Done! See {png_filename} (time: {time.time()-start:0.2f}s)')
+    for i in range(batch_size):
+        png_filename = os.path.join(config.result_dir, f'example_{str(i).zfill(2)}.png')
+        PIL.Image.fromarray(images[i], 'RGB').save(png_filename)
+    print(f'Done! See {config.result_dir} (time: {time.time()-start:0.2f}s)')
 
 
 if __name__ == "__main__":
